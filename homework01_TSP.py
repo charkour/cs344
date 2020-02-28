797ce3686c8bbe41de75edd966360de401f6e3f3
Charles Kornoelje <charleskornoelje@gmail.com>
2020-02-27 21:43:05 -0500

    Finish TSP.

--- homework01_TSP.py
+++ homework01_TSP.py
@@ -1,115 +1,88 @@
-from search import UndirectedGraph, Problem, hill_climbing, simulated_annealing, exp_schedule
+from search import Problem, hill_climbing, simulated_annealing, exp_schedule
+from random import shuffle, randint
 
-print('Here\'s the problem formulation.')
-
-# romania_map = UndirectedGraph(dict(
-#     A=dict(B=10, C=20, D=30),
-#     B=dict(C=40, D=50),
-#     C=dict(D=60)
-# ))
-
-# Create matrix of distances.
+# create matrix of distances.
 distances = {}
 cities = []
 
-n = 5
+n = randint(5, 51)
+
+# initialize matrix
 for city in range(0, n):
     distances[city] = {}
     cities.append(city)
 
+# generate random values
+for city1 in range(0, n):
+    for city2 in range(0, n):
+        if city1 != city2:
+            distances[city1][city2] = distances[city1][city2] = randint(1, 101)
+        else:
+            distances[city1][city2] = distances[city1][city2] = 0
 
-## TODO: Need to make a random number of cities and distances!!!
-
-distances[0][1] = 12
-distances[0][2] = 10
-distances[0][3] = 19
-distances[0][4] = 8
-distances[1][2] = 3
-distances[1][3] = 7
-distances[1][4] = 2
-distances[2][3] = 6
-distances[2][4] = 20
-distances[3][4] = 4
-
-distances[1][0] = 12
-distances[2][0] = 10
-distances[3][0] = 19
-distances[4][0] = 8
-distances[2][1] = 3
-distances[3][1] = 7
-distances[4][1] = 2
-distances[3][2] = 6
-distances[4][2] = 20
-distances[4][3] = 4
-
-random_start_state = [0, 1, 2, 3, 4]
+# referenced: https://stackoverflow.com/questions/976882/shuffling-a-list-of-objects
+shuffle(cities)
 
 
 class TSPProblem(Problem):
+    """TSPProblem inherits the Problem class.
+    Expects the number of cities (int), initial state (int[]), and distances
+    between cities (a dictionary matrix of ints as distances)
+    This problem tries to find the shortest path between all of the points.
+    The problem class wants to maximize the value to multiplying the value
+    by negative one will allow the algorithm to (hopefully) find the optimal (shortest) path.
+    The problem will return a solution as a list of cities, with the origin city only at the start.
+    When calculating the path length, the distance from the last city to the origin is calculated.
+    """
 
-    def __init__(self, num, initial, distances):
+    def __init__(self, num, initial, dist):
         self.n = num
         self.initial = initial
-        self.distances = distances
+        self.distances = dist
 
-    def actions(self, state):
+    def actions(self, curr_state):
+        """Find the possible actions by swapping two cities"""
         actions = []
-        for city1 in range(self.n):
-            for city2 in range(self.n):
-                copy_state = state[:]
-                if city1 != city2:
+        for c1 in range(self.n):
+            for c2 in range(self.n):
+                tmp_state = curr_state[:]
+                if c1 != city2:
                     # Swap two cities
-                    copy_state[city1], copy_state[city2] = copy_state[city2], copy_state[city1]
-                    actions.append(copy_state)
+                    tmp_state[c1], tmp_state[c2] = tmp_state[c2], tmp_state[c1]
+                    actions.append(tmp_state)
         return actions
 
     def result(self, state, action):
-        """Set the new state to the aciton"""
+        """Set the new state to the action"""
         new_state = action
         return new_state
 
-    # def path_cost(self, c, state1, action, state2):
-    #     """Add up all of the distances"""
-    #     cost = 0;
-    #     for n in range(0, self.n):
-    #         cost += self.distances[state2[n]][state2[(n + 1) % self.n]]
-    #     return cost
-
-    # TODO: Need to check the goal state? make sure it is valid?
-
+    # No goal state check because we can never know the optimal (shortest) path
 
     def value(self, state):
-        """Get cost of current state. HC tries to maximize"""
-        # return -1 * self.path_cost(None, None, None, state)
-        cost = 0;
+        """Get cost of current state. Local search tries to maximize
+        So by negating the value, the path will be the shortest possible.
+        """
+        value = 0;
         for n in range(0, self.n):
-            cost += self.distances[state[n]][state[(n + 1) % self.n]]
-        return -1 * cost
+            value += self.distances[state[n]][state[(n + 1) % self.n]]
+        return -1 * value
 
-# https://stackoverflow.com/questions/976882/shuffling-a-list-of-objects
-from random import shuffle
-shuffle(random_start_state)
-print(random_start_state)
+# Setup program
+p = TSPProblem(n, cities, distances)
 
-p = TSPProblem(n, random_start_state, distances)
-
-
-
-# print(cities)
-
-print(p.value(random_start_state))
-print(p.initial)
-
-print(p.actions(random_start_state))
+print('Initial path: ' + str(cities))
+print('Initial value: ' + str(p.value(cities)))
 
 hill_solution = hill_climbing(p)
 print('\nHill-climbing:')
-print('\tSolution:\t' + str(hill_solution))
-print('\tValue:\t\t' + str(p.value(hill_solution)))
+print('\tSolution Path:\t' + str(hill_solution))
+print('\tValue:\t\t\t' + str(p.value(hill_solution)))
+print('\tTotal Length:\t' + str(-1 * p.value(hill_solution)))
 
-# Solve the problem using simulated annealing.
-annealing_solution = \
-    simulated_annealing(p, exp_schedule(k=20, lam=0.005, limit=10000))
+# adapted from u02local/queens.py
+annealing_solution = simulated_annealing(p, exp_schedule(k=20, lam=0.005, limit=10000))
 print('\nSimulated annealing:')
-print('\tSolution:\t' + str(annealing_solution))
-print('\tValue:\t\t' + str(p.value(annealing_solution)))
+print('\tSolution Path:\t' + str(annealing_solution))
+print('\tValue:\t\t\t' + str(p.value(annealing_solution)))
+print('\tTotal Length:\t' + str(-1 * p.value(annealing_solution)))
